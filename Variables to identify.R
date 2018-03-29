86c8287592f808a991ae5dc8e1de4408b774f40a
JackPloshnick <36051309+JackPloshnick@users.noreply.github.com>
2018-03-28 22:47:07 -0500

    defining xfull

--- Variables to identify.R
+++ Variables to identify.R
@@ -1,63 +1,4 @@
 
-#first 4 methods 
-
-Xfull <- model.matrix(~X*treat)
-Xtfull <- model.matrix(~Xt*treatt)
-
-library(glmnet)
-fit1<- cv.glmnet(y = Y, x= Xfull, alpha=1, family='binomial', type='mse')
-#fit2<- cv.glmnet(y = Y, x= Xfull, alpha=0.75, family='binomial', type='mse')
-fit3<- cv.glmnet(y = Y, x= Xfull, alpha=0.5, family='binomial', type='mse')
-fit4<- cv.glmnet(y = Y, x= Xfull, alpha=0, family='binomial', type='mse')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-part2<- wrap.func(covs, approve_bi, treats, covs, approve_bi, treats, speed=F)
-#Do above function, indiviudally, for each test
-
-#Elements that go into function 
-
-covs<- cbind(dem, rep, lib, cons)
-colnames(covs)<- c('Dem', 'Rep', 'Lib', 'Cons')
-
-lib<- ifelse(subset$Q78==4 , 1, 0)
-cons<- ifelse(subset$Q78==2, 1, 0)
-dem<- ifelse(svdat$pid3l=='Dem', 1, 0)
-dem[which(is.na(dem))]<- 0
-rep<- ifelse(svdat$pid3l=='Rep', 1, 0)
-rep[which(is.na(rep))]<- 0
-lib[which(is.na(lib))]<- 0
-cons[which(is.na(cons))]<- 0
-
-approve_bi<- ifelse(svdat$approval<3, 1, 0)
-
-fit1<- cv.glmnet(y = Y, x= Xfull, alpha=1, family='binomial', type='mse')
-
-treats<- cbind(type.mat.final, num.mat.final[,1], stage.mat.final[,1:2],party.mat.final[,1], 
-               along.mat.final[,1:2], type.mat.final[,1:5]*num.mat.final[,1], type.mat.final[,1:5]*stage.mat.final[,1], 
-               type.mat.final[,1:5]*stage.mat.final[,2], type.mat.final[,1:5]*party.mat.final[,1], type.mat.final[,1:5]*along.mat.final[,1],
-               type.mat.final[,1:5]*along.mat.final[,2], num.mat.final[,1]*stage.mat.final[,1], num.mat.final[,1]*stage.mat.final[,2], 
-               num.mat.final[,1]*party.mat.final[,1], num.mat.final[,1]*along.mat.final[,1], num.mat.final[,1]*along.mat.final[,2],
-               stage.mat.final[,1:2]*party.mat.final[,1], stage.mat.final[,1:2]*along.mat.final[,1], 
-               stage.mat.final[,1:2]*along.mat.final[,2], party.mat.final[,1]*along.mat.final[,1], party.mat.final[,1]*along.mat.final[,2] )
-
-
-
-
 #What to do, define every variable in this file 
 
 ######################################################
@@ -117,6 +58,43 @@ fit2<- cv.glmnet(y = Y, x= Xfull, alpha=0.5, family='binomial', type='mse')
 # Elastic Net, Alpha = .2 5
 fit3<- cv.glmnet(y = Y, x= Xfull, alpha=0.15, family='binomial', type='mse')
 fit4<- cv.glmnet(y = Y, x= Xfull, alpha=0, family='binomial', type='mse')
+
+
+### Defining the X variable 
+
+Xfull <- model.matrix(~X*treat)
+X <- covs #line 432 of repcode 
+covs<- cbind(dem, rep, lib, cons) #line 373 of rep code 
+
+dem<- ifelse(svdat$pid3l=='Dem', 1, 0) Line #line 366-369 of rep code
+dem[which(is.na(dem))]<- 0
+rep<- ifelse(svdat$pid3l=='Rep', 1, 0)
+rep[which(is.na(rep))]<- 0
+
+cons<- ifelse(svdat$ideo3<3, 1, 0) #line 230-231 of rep code
+lib<- ifelse(svdat$ideo3==4|svdat$ideo3==5, 1, 0)
+
+lib[which(is.na(lib))]<- 0 #line 370-371 of rep code 
+cons[which(is.na(cons))]<- 0
+
+treat<- treats #line 448 of rep code 
+
+treats<- cbind(type.mat.final, num.mat.final[,1], stage.mat.final[,1:2],party.mat.final[,1], 
+               along.mat.final[,1:2], type.mat.final[,1:5]*num.mat.final[,1], type.mat.final[,1:5]*stage.mat.final[,1], 
+               type.mat.final[,1:5]*stage.mat.final[,2], type.mat.final[,1:5]*party.mat.final[,1], type.mat.final[,1:5]*along.mat.final[,1],
+               type.mat.final[,1:5]*along.mat.final[,2], num.mat.final[,1]*stage.mat.final[,1], num.mat.final[,1]*stage.mat.final[,2], 
+               num.mat.final[,1]*party.mat.final[,1], num.mat.final[,1]*along.mat.final[,1], num.mat.final[,1]*along.mat.final[,2],
+               stage.mat.final[,1:2]*party.mat.final[,1], stage.mat.final[,1:2]*along.mat.final[,1], 
+               stage.mat.final[,1:2]*along.mat.final[,2], party.mat.final[,1]*along.mat.final[,1], party.mat.final[,1]*along.mat.final[,2] )
+
+## line 391 of rep code 
+
+
+
+
+## unclear what exaclty the ~ means. 
+
+
 
 
 ######### FindIt ########
