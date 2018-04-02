bf7998bd49f5b12b9a6b3cbe6c67a57a4f70aeca
Emily Garner <emilyg1995@yahoo.com>
2018-04-02 14:11:06 -0500

    Test 8 & Updated Subsets
    updated the subset coding so that they all work properly and got test 8 working with subsets

--- Portion Tests.R
+++ Portion Tests.R
@@ -4,7 +4,22 @@
 
 ### original calculation from variables to identify:
 
-## defining xtfull
+## defining variables
+
+treats<- cbind(type.mat.final, num.mat.final[,1], stage.mat.final[,1:2],party.mat.final[,1], 
+               along.mat.final[,1:2], type.mat.final[,1:5]*num.mat.final[,1], type.mat.final[,1:5]*stage.mat.final[,1], 
+               type.mat.final[,1:5]*stage.mat.final[,2], type.mat.final[,1:5]*party.mat.final[,1], type.mat.final[,1:5]*along.mat.final[,1],
+               type.mat.final[,1:5]*along.mat.final[,2], num.mat.final[,1]*stage.mat.final[,1], num.mat.final[,1]*stage.mat.final[,2], 
+               num.mat.final[,1]*party.mat.final[,1], num.mat.final[,1]*along.mat.final[,1], num.mat.final[,1]*along.mat.final[,2],
+               stage.mat.final[,1:2]*party.mat.final[,1], stage.mat.final[,1:2]*along.mat.final[,1], 
+               stage.mat.final[,1:2]*along.mat.final[,2], party.mat.final[,1]*along.mat.final[,1], party.mat.final[,1]*along.mat.final[,2] )
+
+
+treat<- treats #line 448 of rep code 
+
+covs<- cbind(dem, rep, lib, cons) #line 373 of rep code 
+X <- covs #line 432 of repcode 
+Xfull <- model.matrix(~X*treat)
 
 Xt<- covs #line 432 of rep code 
 #covs is defined in lasso section 
@@ -14,15 +29,31 @@ treatt<- treats #line 432 of rep code
 
 Xtfull <- model.matrix(~Xt*treatt) #line 56 of SLF
 
+Y<- approve_bi<- ifelse(svdat$approval<3, 1, 0) #line 292 of rep code 
+
 #install.packages("BayesTree")
 library(BayesTree)
 fit8<- bart(x.train=Xfull, y.train=factor(Y), x.test=Xtfull, ndpost=1000, nskip=500, usequants=T)
 
+## with Xfull twice instead of Xtfull b/c they seem the same? Works...
+fit8<- bart(x.train=Xfull, y.train=factor(Y), x.test=Xfull, ndpost=1000, nskip=500, usequants=T)
+
 ### test on portion:
 
+covstest_1 <- covs[c(1:107),]
+covsfull_1 <- covs[-c(1:107),]
 
+treattest_1 <- treat[c(1:107),]
+treatfull_1 <-treat[-c(1:107),]
 
-fit8<- bart(x.train=Xfull, y.train=factor(Y), x.test=Xtfull, ndpost=1000, nskip=500, usequants=T)
+Xfull_1 <-model.matrix(~covsfull_1*treatfull_1)
+
+Xtest_1 <- model.matrix(~covstest_1*treattest_1)
+
+Yfull_1<- ifelse(svdat[-c(1:107),]$approval<3, 1, 0)
+
+fit8<- bart(x.train=Xfull_1, y.train=factor(Yfull_1), x.test=Xtest_1, ndpost=1000, nskip=500, usequants=T)
+## works!
 
 
 
--- test partitions.R
+++ test partitions.R
@@ -1,30 +1,9 @@
 #making 10 portions 
 
 
-
-##############
-#defining x varaible in portions 
-
-Xfull <- model.matrix(~covs*treat)
-
-Xfull_1 <-model.matrix(~covsfull_1*treatfull_1)
-
-xtest<- model.matrix(~covstest_1*treattest_1)
-
-fit8<- bart(x.train=Xfull, y.train=factor(Y), x.test=Xtfull, ndpost=1000, nskip=500, usequants=T)
-
-
-fit8<- bart(x.train=Xfull_1, y.train=factor(Yfull_1), x.test=xtest_1, ndpost=1000, nskip=500, usequants=T)
-##doesn't work
-
-
-
 #Def of 1/10 for covs
 
-
-
 covstest_1 <- covs[c(1:107),]
-
 covstest_2<- covs[c(108:215),]
 covstest_3<- covs[c(216:323),]
 covstest_4<- covs[c(324:431),]
@@ -40,21 +19,22 @@ covstest<- cbind(covsfull_1,covsfull_2,covsfull_3,covsfull_4,covsfull_5,covsfull
 
 # Def of 9/10 for covs
 
-covsfull_1 <- covs[-covstest_1,]
-covsfull_2 <- covs[-covstest_2,]
-covsfull_3 <- covs[-covstest_3,]
-covsfull_4 <- covs[-covstest_4,]
-covsfull_5 <- covs[-covstest_5,]
-covsfull_6 <- covs[-covstest_6,]
-covsfull_7 <- covs[-covstest_7,]
-covsfull_8 <- covs[-covstest_8,]
-covsfull_9 <- covs[-covstest_9,]
-covsfull_10 <- covs[-covstest_10,]
+covsfull_1 <- covs[-(1:107),]
+covsfull_2 <- covs[-c(108:215),]
+covsfull_3 <- covs[-c(216:323),]
+covsfull_4 <- covs[-c(324:431),]
+covsfull_5 <- covs[-c(432:539),]
+covsfull_6 <- covs[-c(540:647),]
+covsfull_7 <- covs[-c(648:755),]
+covsfull_8 <- covs[-c(756:863),]
+covsfull_9 <- covs[-c(864:971),]
+covsfull_10 <- covs[-c(972:1074),]
 
 covsfull<-cbind(covsfull_1,covsfull_2,covsfull_3,covsfull_4,covsfull_5,covsfull_6,covsfull_7,covsfull_8,covsfull_9,covsfull_10)
 
 
 ####def of 1/10 for treat
+
 treattest_1 <- treat[c(1:107),]
 treattest_2<- treat[c(108:215),]
 treattest_3<- treat[c(216:323),]
@@ -71,20 +51,20 @@ treattest<- cbind(treattest_2,treattest_3,treattest_4,treattest_5,treattest_6,tr
 
 # Def of 9/10 for treat
 
-treatfull_1 <-treat[-treattest_1,]
-treatfull_2 <- treat[-treattest_2,]
-treatfull_3 <- treat[-treattest_3,]
-treatfull_4 <- treat[-treattest_4,]
-treatfull_5 <- treat[-treattest_5,]
-treatfull_6 <- treat[-treattest_6,]
-treatfull_7 <- treat[-treattest_7,]
-treatfull_8 <- treat[-treattest_8,]
-treatfull_9 <- treat[-treattest_9,]
-treatfull_10 <- treat[-treattest_10,]
+treatfull_1 <-treat[-c(1:107),]
+treatfull_2 <- treat[-c(108:215),]
+treatfull_3 <- treat[-c(216:323),]
+treatfull_4 <- treat[-c(324:431),]
+treatfull_5 <- treat[-c(432:539),]
+treatfull_6 <- treat[-c(540:647),]
+treatfull_7 <- treat[-c(648:755),]
+treatfull_8 <- treat[-c(756:863),]
+treatfull_9 <- treat[-c(864:971),]
+treatfull_10 <- treat[-c(972:1074),]
 
 treatfull <- 
   
-  #################
+#################
 #def y in portions 
 
 Yfull_1<- ifelse(svdat[-c(1:107),]$approval<3, 1, 0)
@@ -99,12 +79,11 @@ Yfull_9<- ifelse(svdat[-c(864:971),]$approval<3, 1, 0)
 Yfull_10<- ifelse(svdat[-c(972:1074),]$approval<3, 1, 0)
 
 
-
-
 Yfull <-cbind(Yfull_1,Yfull_2,Yfull_3,Yfull_4,Yfull_5,Yfull_6,Yfull_7,Yfull_8,Yfull_9, Yfull_10)
 
 
-#####
+#################
+#full data subsets
 
 first<- svdat[c(1:107),]
 second<- svdat[c(108:215),]
