25ca0e7523036a07210eaa71d8d854c8c10ac6ae
jgross962 <36051238+jgross962@users.noreply.github.com>
2018-04-16 16:52:16 -0500

    Debugged Boostrap

--- Bootstrap_OOSPredicts.R
+++ Bootstrap_OOSPredicts.R
@@ -16,27 +16,22 @@ OOSPredicts = function(x,y, test.indexes = sample(length(y),as.integer(length(y)
     return(ff)
   }
   
-  #library(glmnet)
-  
   # For fits 1,2, 3 we optimize the hyperparameters once, for efficiency (loses some accuracy)
   # Lasso
   fit1<- glmnet(x = x.train, y = y.train, alpha=1, family='binomial')
   fit1.predict = predict(fit1, s= best.lambda1, newx = x.test)
   fit1.logistPred = logist(fit1.predict)
-  fit1.logistPred.RMSE = sqrt(mean((fit1.logistPred-y.test)^2))
   
   
   # Elastic Net, Alpha = .5
   fit2<- glmnet(y = y.train, x= x.train, alpha=0.5, family='binomial')
   fit2.predict = predict(fit2, s= best.lambda2, newx = x.test)
   fit2.logistPred = logist(fit2.predict)
-  fit2.logistPred.RMSE = sqrt(mean((fit2.logistPred-y.test)^2))
   
   # Elastic Net, Alpha = .25
   fit3<- glmnet(y = y.train, x= x.train, alpha=0.25, family='binomial')
-  fit3.predict = predict(fit3, s= best.lambda, newx = x.test)
+  fit3.predict = predict(fit3, s= best.lambda3, newx = x.test)
   fit3.logistPred = logist(fit3.predict)
-  fit3.logistPred.RMSE = sqrt(mean((fit3.logistPred-y.test)^2))
   
   # Fit4 not published in paper -- so not doing it
   #fit4<- cv.glmnet(y = Y, x= Xfull, alpha=0, family='binomial', type='mse')
@@ -44,46 +39,41 @@ OOSPredicts = function(x,y, test.indexes = sample(length(y),as.integer(length(y)
   ## Skipping FindIt since documentation changed (per)
   
   # Bayesian GLM -- Revisit -- probably not working right
-  #library(arm)
   fit6<- bayesglm(y.train~x.train-1, family=binomial(link=logit))
   fit6.predict = logist(x.test%*%fit6$coefficients)
-  fit6.logistPred.RMSE = sqrt(mean((fit6.predict-y.test)^2))
   
   # Fit 7 = Boosted Trees is not published ### SKipping
   
   # Fit 8 = BART
-  #library(BayesTree)
   fit8<- bart(x.train=x.train, y.train=factor(y.train), x.test=x.test, ndpost=1000, nskip=500, usequants=T)
   fit8.pred<- pnorm(apply(fit8$yhat.test, 2, mean))
-  fit8.rmse = sqrt(mean((fit8.pred-y.test)^2))
   
   # Fit 9  = RandomForest
-  #library(randomForest)
   fit9<- randomForest(y = factor(y.train), x = x.train)
   X.test.forest = x.test
-  `colnames<-`(X.test.forest,colnames(x.train))
+  colnames(X.test.forest) = colnames(x.train)
   fit9.pred.raw = predict(fit9,newdata = X.test.forest,type = "prob" )
   fit9.pred = fit9.pred.raw[,2]
-  fit9.rmse = sqrt(mean((fit9.pred-y.test)^2))
   
   
   # Fit 10 = Skipped in Paper and SLF_round2 code
   
   # Fit 11 = KRLS
-  #library(KRLS)
-  fit11<- krls(X = x.train[,-1], y = y.train, derivative=F)
+  # Gets errors if all vals in column have same value (i.e. all 0 or all 1)
+  x.train.krls = x.train[,-1]
+  sums = as.numeric(apply(x.train.krls,2,sum))
+  bad.KRLS.index = which(sums==0)
+  bad.KRLS.index = c(bad, which(sums==dim(x.train.krls)[1]))
+  x.train.krls = x.train.krls[,-bad.KRLS.index]
+  fit11<- krls(X = x.train.krls, y = y.train, derivative=F)
   fit11.predict = predict(fit11,newdata = x.test[,-1])$fit
-  fit11.rmse = sqrt(mean((fit11.predict-y.test)^2))
   
   # Fit 12 = SVM-SMO
-   #library(rJava)
    .jinit(parameters="-Xmx4g")
-   #library(RWeka)
    subset.index = (1:length(y))[-test.indexes]
    fit12 <- SMO(y ~ ., data = data.frame(y=factor(y),x), control = Weka_control(M = TRUE ) , subset = subset.index)
    fit12.predict =predict(fit12, newdata= data.frame(x[test.indexes,]), type="probability" )[,2]
-   fit12.RMSE = sqrt(mean((fit12.predict-y.test)^2))
-
+   
 
   # Fit 13 = Simple Mean
   fit13.predict = mean(y.train)
@@ -96,11 +86,9 @@ OOSPredicts = function(x,y, test.indexes = sample(length(y),as.integer(length(y)
   
   # W/ Fit 12 (Java)
   models = rbind("Lasso", "Elastic Net (a = .5)","Elastic Net (a = .25)", "Bayesian GLM", "BART", "Random Forest", "KRLS", "SVM_SMO", "Simple Average")
-  Preds.All = cbind(fit1.logistPred,fit2.logistPred,fit3.logistPred,fit6.predict,fit8.pred,fit9.pred,fit11.predict, fit12.predict,  fit13.predict)
+  Preds.All = cbind(fit1.logistPred, fit2.logistPred ,fit3.logistPred,fit6.predict,fit8.pred,fit9.pred,fit11.predict, fit12.predict,  fit13.predict)
   
-  #RMSE.all = rbind(fit1.logistPred.RMSE,fit2.logistPred.RMSE,fit3.logistPred.RMSE,fit6.logistPred.RMSE,fit8.rmse,fit9.rmse,fit11.rmse,fit12.RMSE, fit13.RSME)
   colnames(Preds.All) = models
-  #return(data.frame(RMSE.all))
   return(Preds.All)
 }
 
@@ -231,15 +219,18 @@ fit3<- cv.glmnet(y = y.train, x= x.train, alpha=0.25, family='binomial', type='m
 best.lambda3 = fit3$lambda.min
 
 ########################### Run Bootstapping #################################
-num.boostraps = 1
+num.boostraps = 500
 # For repeatable but still meaningful results, set two seeds, allowing each bootstrap sample to be different but still random
-set.seed(100)
+set.seed(10)
 seednum = sample(10000,num.boostraps)
 results = array(dim = c(1074,9,num.boostraps))
 # Measure Run Time
 start.time = Sys.time()
+
+debugchecker = numeric()
+debugchecker.inner = numeric()
 for (i in 1:num.boostraps){
-  
+  debugchecker = i
   set.seed(seednum[i])
   bootstramp.sample.indexes = sample(1074,1074,replace = TRUE)
 
@@ -255,17 +246,40 @@ for (i in 1:num.boostraps){
   indexes.matrix.last = matrix(indexes.last, nrow = 1)
   
   preds = matrix(data = NA, ncol = 9);
-  for(i in 1:9){
-    temp <-OOSPredicts(Xfull,Y,indexes.matrix.first[i,])
+  for(j in 1:9){
+    debugchecker.inner=j
+    temp <-OOSPredicts(Xfull,Y,indexes.matrix.first[j,])
     preds = rbind(preds,temp)
   }
-  
+  debugchecker.inner=10
   last<- OOSPredicts(Xfull,Y,indexes.matrix.last)
   preds = rbind(preds,last)
   
   preds.in.order = preds[order(as.numeric(rownames(preds))),]
+  # Has a  row of all N/As at end that needs to be deleted
+  preds.in.order = preds.in.order[1:1074,]
   results[,,i] = preds.in.order;
 }
 finish.time = Sys.time()
 
 run.time = finish.time-start.time
+
+
+############# Jon's Debugging
+allchecks = numeric()
+bad = numeric()
+for ( i in 2:280){
+  check = (sum(as.numeric(x.train[,i])))
+  allchecks = c(allchecks, check)
+  if (check==0){
+   bad = c(bad,i) 
+  }
+  if (check ==967){
+    bad = c(bad,i)
+  }
+  
+}
+allchecks
+bad
+
+colnames(Xfull)[138]
