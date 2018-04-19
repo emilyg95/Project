93965a77e9277a14e42b39d8c7274936e2abf33c
Emily Garner <emilyg1995@yahoo.com>
2018-04-19 14:13:49 -0500

    Everything consolidated
    hopefully everything you need to run everything is in here

--- EBMA_Outputs.R
+++
@@ -1,7 +0,0 @@
-install.packages("EBMAforecast")
-library(EBMAforecast)
-Names = c("Lasso", "Elastic Net a = .5", "Elastic Net a = .25", "Bayesian GLM", "BART", "Random Forest", "KRLS", "SVM_SMO", "Simple Average")
-ForecastData = makeForecastData(.predCalibration = preds.in.order, .outcomeCalibration = Y, .modelNames = Names)
-myCal<-calibrateEnsemble(ForecastData)
-myCal@modelWeights
-str(myCal)
--- FINAL.R
+++ FINAL.R
@@ -1,3 +1,98 @@
+load("Het_Experiment.Rdata")
+
+#############################  Defining Variables ############################### 
+
+## dem, rep, cons, lib
+
+dem<- ifelse(svdat$pid3l=='Dem', 1, 0)  #line 366-369 of rep code
+dem[which(is.na(dem))]<- 0
+rep<- ifelse(svdat$pid3l=='Rep', 1, 0)
+rep[which(is.na(rep))]<- 0
+
+cons<- ifelse(svdat$ideo3<3, 1, 0) #line 230-231 of rep code
+lib<- ifelse(svdat$ideo3==4|svdat$ideo3==5, 1, 0)
+
+lib[which(is.na(lib))]<- 0 #line 370-371 of rep code 
+cons[which(is.na(cons))]<- 0
+
+
+## treats
+
+type.mat<- matrix(0, nrow = 1074, ncol=7)
+colnames(type.mat)<- sort(unique(as.character(svdat$cond.type)))
+for(z in 1:nrow(type.mat)){
+  type.mat[z,which(colnames(type.mat)==svdat$cond.type[z])]<- 1
+}
+
+type.mat.final<- type.mat[,-1]
+
+types<- sort(unique(as.character(svdat$cond.type)))
+type.num<- match(svdat$cond.type, types)
+number<- c('control', '$20 million', '$50 thousand')
+amount.num<- match(svdat$cond.money, number)
+request<- c('control', 'requested', 'secured', 'will request')
+stage.num<- match(svdat$cond.stage, request)
+party<- c('control', 'a Republican', 'a Democrat')
+party.num<- match(svdat$cond.party, party)
+along<- c('control', 'alone', 'w/ Rep', 'w/ Dem')
+along.num<- match(svdat$cond.alongWith, along)
+
+num.mat<- matrix(0, nrow=1074, ncol=3)
+colnames(num.mat)<- number
+for(z in 1:nrow(num.mat)){
+  num.mat[z,which(colnames(num.mat)==svdat$cond.money[z])]<- 1
+}
+
+num.mat.final<- num.mat[,-1]
+
+stage.mat<- matrix(0, nrow=1074, ncol=4)
+colnames(stage.mat)<- request
+for(z in 1:nrow(stage.mat)){
+  stage.mat[z,which(colnames(stage.mat)==svdat$cond.stage[z])]<- 1
+}
+
+stage.mat.final<- stage.mat[,-1]
+
+party.mat<- matrix(0, nrow=1074, ncol=3)
+colnames(party.mat)<- party
+for(z in 1:nrow(party.mat)){
+  party.mat[z, which(colnames(party.mat)==svdat$cond.party[z])]<- 1
+}
+
+party.mat.final<- party.mat[,-1]	
+
+along.mat<- matrix(0, nrow=1074, ncol=4)
+colnames(along.mat)<- 	along
+for(z in 1:nrow(along.mat)){
+  along.mat[z,which(colnames(along.mat)==svdat$cond.alongWith[z])]<- 1
+}
+
+along.mat.final<- along.mat[,-1]	
+
+treats<- cbind(type.mat.final, num.mat.final[,1], stage.mat.final[,1:2],party.mat.final[,1], 
+               along.mat.final[,1:2], type.mat.final[,1:5]*num.mat.final[,1], type.mat.final[,1:5]*stage.mat.final[,1], 
+               type.mat.final[,1:5]*stage.mat.final[,2], type.mat.final[,1:5]*party.mat.final[,1], type.mat.final[,1:5]*along.mat.final[,1],
+               type.mat.final[,1:5]*along.mat.final[,2], num.mat.final[,1]*stage.mat.final[,1], num.mat.final[,1]*stage.mat.final[,2], 
+               num.mat.final[,1]*party.mat.final[,1], num.mat.final[,1]*along.mat.final[,1], num.mat.final[,1]*along.mat.final[,2],
+               stage.mat.final[,1:2]*party.mat.final[,1], stage.mat.final[,1:2]*along.mat.final[,1], 
+               stage.mat.final[,1:2]*along.mat.final[,2], party.mat.final[,1]*along.mat.final[,1], party.mat.final[,1]*along.mat.final[,2] )
+
+treat<- treats #line 448 of rep code 
+
+
+## X variable 
+
+covs<- cbind(dem, rep, lib, cons) #line 373 of rep code 
+X <- covs #line 432 of repcode 
+Xfull <- model.matrix(~X*treat)
+
+
+#Defining the Y variable 
+
+Y<- approve_bi<- ifelse(svdat$approval<3, 1, 0) #line 292 of rep code 
+
+
+
 ############################ Define Function to make OOS Predicts #########################################################
 
 OOSPredicts = function(x,y, test.indexes = sample(length(y),as.integer(length(y)/10))){
@@ -69,20 +164,20 @@ OOSPredicts = function(x,y, test.indexes = sample(length(y),as.integer(length(y)
   bad.KRLS.index = c(bad.KRLS.index, which(sums==dim(x.train.krls)[1]))
   
   if (length(bad.KRLS.index)>0){
-  x.train.krls = x.train.krls[,-bad.KRLS.index]
-  x.test.krls = x.test.krls[,-bad.KRLS.index]
+    x.train.krls = x.train.krls[,-bad.KRLS.index]
+    x.test.krls = x.test.krls[,-bad.KRLS.index]
   }
   fit11<- krls(X = x.train.krls, y = y.train, derivative=F)
-
+  
   fit11.predict = predict(fit11,newdata = x.test.krls )$fit
   
   # Fit 12 = SVM-SMO
-   .jinit(parameters="-Xmx4g")
-   subset.index = (1:length(y))[-test.indexes]
-   fit12 <- SMO(y ~ ., data = data.frame(y=factor(y),x), control = Weka_control(M = TRUE ) , subset = subset.index)
-   fit12.predict =predict(fit12, newdata= data.frame(x[test.indexes,]), type="probability" )[,2]
-   
-
+  .jinit(parameters="-Xmx4g")
+  subset.index = (1:length(y))[-test.indexes]
+  fit12 <- SMO(y ~ ., data = data.frame(y=factor(y),x), control = Weka_control(M = TRUE ) , subset = subset.index)
+  fit12.predict =predict(fit12, newdata= data.frame(x[test.indexes,]), type="probability" )[,2]
+  
+  
   # Fit 13 = Simple Mean
   fit13.predict = mean(y.train)
   fit13.RSME= sqrt(mean((fit13.predict-y.test)^2))
@@ -90,7 +185,7 @@ OOSPredicts = function(x,y, test.indexes = sample(length(y),as.integer(length(y)
   # # W/O FIt 12 (Java)
   # models = rbind("Lasso", "Elastic Net (a = .5)","Elastic Net (a = .25)", "Bayesian GLM", "BART", "Random Forest", "KRLS" , "Simple Average")#"SVM_SMO"
   # Preds.All = cbind(fit1.logistPred,fit2.logistPred,fit3.logistPred,fit6.predict,fit8.pred,fit9.pred,fit11.predict,  fit13.predict)#fit12
- 
+  
   
   # W/ Fit 12 (Java)
   models = rbind("Lasso", "Elastic Net (a = .5)","Elastic Net (a = .25)", "Bayesian GLM", "BART", "Random Forest", "KRLS", "SVM_SMO", "Simple Average")
@@ -100,100 +195,7 @@ OOSPredicts = function(x,y, test.indexes = sample(length(y),as.integer(length(y)
   return(Preds.All)
 }
 
- 
 
-####################################### Defining Variables #####################################
-
-setwd("C:/Users/ASME/Documents/GitHub/Project/")
-#setwd("C:/Users/jgros/documents/GitHub/Project/")
-load("Het_Experiment.Rdata")
-
-dem<- ifelse(svdat$pid3l=='Dem', 1, 0)  #line 366-369 of rep code
-dem[which(is.na(dem))]<- 0
-rep<- ifelse(svdat$pid3l=='Rep', 1, 0)
-rep[which(is.na(rep))]<- 0
-
-cons<- ifelse(svdat$ideo3<3, 1, 0) #line 230-231 of rep code
-lib<- ifelse(svdat$ideo3==4|svdat$ideo3==5, 1, 0)
-
-lib[which(is.na(lib))]<- 0 #line 370-371 of rep code 
-cons[which(is.na(cons))]<- 0
-
-
-#### Defining treats
-
-type.mat<- matrix(0, nrow = 1074, ncol=7)
-colnames(type.mat)<- sort(unique(as.character(svdat$cond.type)))
-for(z in 1:nrow(type.mat)){
-  type.mat[z,which(colnames(type.mat)==svdat$cond.type[z])]<- 1
-}
-
-type.mat.final<- type.mat[,-1]
-
-
-types<- sort(unique(as.character(svdat$cond.type)))
-type.num<- match(svdat$cond.type, types)
-number<- c('control', '$20 million', '$50 thousand')
-amount.num<- match(svdat$cond.money, number)
-request<- c('control', 'requested', 'secured', 'will request')
-stage.num<- match(svdat$cond.stage, request)
-party<- c('control', 'a Republican', 'a Democrat')
-party.num<- match(svdat$cond.party, party)
-along<- c('control', 'alone', 'w/ Rep', 'w/ Dem')
-along.num<- match(svdat$cond.alongWith, along)
-
-
-num.mat<- matrix(0, nrow=1074, ncol=3)
-colnames(num.mat)<- number
-for(z in 1:nrow(num.mat)){
-  num.mat[z,which(colnames(num.mat)==svdat$cond.money[z])]<- 1
-}
-num.mat.final<- num.mat[,-1]
-
-stage.mat<- matrix(0, nrow=1074, ncol=4)
-colnames(stage.mat)<- request
-for(z in 1:nrow(stage.mat)){
-  stage.mat[z,which(colnames(stage.mat)==svdat$cond.stage[z])]<- 1
-}
-
-stage.mat.final<- stage.mat[,-1]
-
-party.mat<- matrix(0, nrow=1074, ncol=3)
-colnames(party.mat)<- party
-for(z in 1:nrow(party.mat)){
-  party.mat[z, which(colnames(party.mat)==svdat$cond.party[z])]<- 1
-}
-
-party.mat.final<- party.mat[,-1]	
-
-along.mat<- matrix(0, nrow=1074, ncol=4)
-colnames(along.mat)<- 	along
-for(z in 1:nrow(along.mat)){
-  along.mat[z,which(colnames(along.mat)==svdat$cond.alongWith[z])]<- 1
-}
-
-along.mat.final<- along.mat[,-1]	
-
-
-treats<- cbind(type.mat.final, num.mat.final[,1], stage.mat.final[,1:2],party.mat.final[,1], 
-               along.mat.final[,1:2], type.mat.final[,1:5]*num.mat.final[,1], type.mat.final[,1:5]*stage.mat.final[,1], 
-               type.mat.final[,1:5]*stage.mat.final[,2], type.mat.final[,1:5]*party.mat.final[,1], type.mat.final[,1:5]*along.mat.final[,1],
-               type.mat.final[,1:5]*along.mat.final[,2], num.mat.final[,1]*stage.mat.final[,1], num.mat.final[,1]*stage.mat.final[,2], 
-               num.mat.final[,1]*party.mat.final[,1], num.mat.final[,1]*along.mat.final[,1], num.mat.final[,1]*along.mat.final[,2],
-               stage.mat.final[,1:2]*party.mat.final[,1], stage.mat.final[,1:2]*along.mat.final[,1], 
-               stage.mat.final[,1:2]*along.mat.final[,2], party.mat.final[,1]*along.mat.final[,1], party.mat.final[,1]*along.mat.final[,2] )
-
-
-treat<- treats #line 448 of rep code 
-
-
-### Defining the X variable 
-covs<- cbind(dem, rep, lib, cons) #line 373 of rep code 
-X <- covs #line 432 of repcode 
-Xfull.original <- model.matrix(~X*treat)
-
-#Defining the Y variable 
-Y.original<- approve_bi<- ifelse(svdat$approval<3, 1, 0) #line 292 of rep code 
 
 ############################ Setup for ML ########################
 
@@ -226,7 +228,10 @@ best.lambda2 = fit2$lambda.min
 fit3<- cv.glmnet(y = Y.original, x= Xfull.original, alpha=0.25, family='binomial', type='mse')
 best.lambda3 = fit3$lambda.min
 
+
+
 ########################### Run Bootstrapping #################################
+
 num.boostraps = 500
 # For repeatable but still meaningful results, set two seeds, allowing each bootstrap sample to be different but still random
 set.seed(10)
@@ -241,7 +246,7 @@ for (i in 1:num.boostraps){
   debugchecker = i
   set.seed(seednum[i])
   bootstramp.sample.indexes = sample(1074,1074,replace = TRUE)
-
+  
   Xfull = Xfull.original[bootstramp.sample.indexes,]
   Y = Y.original[bootstramp.sample.indexes]
   
@@ -273,21 +278,39 @@ finish.time = Sys.time()
 run.time = finish.time-start.time
 
 
-############# Jon's Debugging
-allchecks = numeric()
-bad = numeric()
-for ( i in 2:280){
-  check = (sum(as.numeric(x.train[,i])))
-  allchecks = c(allchecks, check)
-  if (check==0){
-   bad = c(bad,i) 
-  }
-  if (check ==967){
-    bad = c(bad,i)
-  }
-  
-}
-allchecks
-bad
 
-colnames(Xfull)[138]
+########################### Run Superlearners #################################
+
+# Montgomery
+
+install.packages("EBMAforecast")
+library(EBMAforecast)
+Names = c("Lasso", "Elastic Net a = .5", "Elastic Net a = .25", "Bayesian GLM", "BART", "Random Forest", "KRLS", "SVM_SMO", "Simple Average")
+ForecastData = makeForecastData(.predCalibration = preds.in.order, .outcomeCalibration = Y, .modelNames = Names)
+myCal<-calibrateEnsemble(ForecastData)
+myCal@modelWeights
+str(myCal)
+
+
+# Original Paper
+
+regress.func <- function(Y, preds.var){
+  
+  # need to smartly figure out which columns are not NA 
+  orgcols <- length(preds.var[1,])
+  notNA <- which(!is.na(preds.var[1,]))
+  predX <- preds.var[,notNA ]
+  
+  library(quadprog)
+  d.mat <- solve(chol(t(predX)%*%predX))
+  a.mat <- cbind(rep(1, ncol(predX)), diag(ncol(predX)))
+  b.vec <- c(1, rep(0, ncol(predX)))
+  d.vec <- t(Y) %*% predX
+  out<- solve.QP(Dmat = d.mat, factorized =TRUE, dvec = d.vec, Amat = a.mat, bvec = b.vec, meq = 1)
+  coefs <- rep(NA, orgcols)
+  notDel <- c(1:orgcols)[notNA]#[notCor]
+  coefs[notDel] <- out$solution
+  return(coefs)
+}
+
+
