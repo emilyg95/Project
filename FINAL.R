d30468fcd50d5a99224f527dc6cc28c3912e5b2e
Emily Garner <emilyg1995@yahoo.com>
2018-04-24 13:49:46 -0500

    Montgomery's function weights
    code to weight montgomery's function when we have the Y matrix

--- FINAL.R
+++ FINAL.R
@@ -290,8 +290,16 @@ ForecastData = makeForecastData(.predCalibration = preds.in.order, .outcomeCalib
 myCal<-calibrateEnsemble(ForecastData)
 myCal@modelWeights
 
-
-
+# matrix of predicted weights
+data_out <- data.frame()
+for(i in 1:500){
+  Names <- c("Lasso", "Elastic Net a = .5", "Elastic Net a = .25", "Bayesian GLM", "BART", "Random Forest", "KRLS", "SVM_SMO", "Simple Average")
+  results_slice <- results[,,i]
+  ForecastData <- makeForecastData(.predCalibration = results_slice, .outcomeCalibration = Y, .modelNames = Names)
+  myCal <- calibrateEnsemble(ForecastData)
+  weights <- myCal@modelWeights
+  rbind(data_out, weights)
+}
 
 
 # Original Paper
