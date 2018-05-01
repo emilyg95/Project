90ac4cb984b30bd4f1eb6f11b5294d3f50501940
JackPloshnick <36051309+JackPloshnick@users.noreply.github.com>
2018-05-01 14:59:25 -0500

    Finished Final Plot

--- making plots.R
+++ making plots.R
@@ -57,7 +57,8 @@ error_no7
 
 plotting_data_no7<- as.data.frame(mean.coefs_no7)
 
-Names_no7 <- c("Lasso", "Elastic Net a = .5", "Elastic Net a = .25", "Bayesian GLM", "BART", "Random Forest", "KRLS", "Simple Average")
+Names_no7 <- c("Lasso", "Elastic Net a = .5", "Elastic Net a = .25", "Bayesian GLM", "BART",
+               "Random Forest", "KRLS", "Simple Average")
 
 plotting_data_no7 <- cbind(Names_no7, plotting_data_no7)
 
@@ -67,7 +68,7 @@ lower_no7<- (mean.coefs_no7 - 1.96*error_no7)
 ggplot(plotting_data_no7, aes(x = plotting_data_no7$Names_no7, y = plotting_data_no7$mean.coefs)) +
   geom_point(size = 3) +
   geom_errorbar(aes(ymax = upper_no7, ymin = lower_no7))+ 
-  labs(title = "Model weights of Regression Ensamble \n(missing model KRLS)")+
+  labs(title = "Model Weights of Regression Ensamble \n(missing Model KRLS)")+
   ylab("Model Weights")+
   xlab("Model") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))
@@ -128,3 +129,74 @@ ggplot(plotting_data, aes(x = plotting_data$Names, y = plotting_data$mean.coefs)
   labs(title = "Model weights of Regression Ensamble ")+
   ylab("Model Weights")+
   xlab("Model")
+
+
+####### Plotting Montgomery data 
+mean_coefs_montgomery_no7
+error_montgomery_no7
+
+
+plotting_data_montgomery<- as.data.frame(mean_coefs_montgomery_no7)
+
+Names_no7 <- c("Lasso", "Elastic Net a = .5", "Elastic Net a = .25", "Bayesian GLM", "BART",
+               "Random Forest", "KRLS", "Simple Average")
+
+plotting_data_montgomery <- cbind(Names_no7, plotting_data_montgomery)
+upper_montgomery<-(mean_coefs_montgomery_no7+ 1.96*error_montgomery_no7)
+lower_montgomery<- (mean_coefs_montgomery_no7 - 1.96*error_montgomery_no7)
+
+ggplot(plotting_data_montgomery, aes(x = plotting_data_montgomery$Names, y = plotting_data_montgomery$mean_coefs_montgomery_no7)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymax = upper_montgomery, ymin = lower_montgomery))  +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))+ 
+  labs(title = "Model weights of  EBMA")+
+  ylab("Model Weights")+
+  xlab("Model")
+
+######## one plot 
+
+colnames(plotting_data_montgomery)[2] <- "coef"
+plotting_data_montgomery$type <- "EBMA"
+plotting_data_montgomery <- cbind(plotting_data_montgomery, error_montgomery_no7)
+colnames(plotting_data_montgomery)[4] <- "error"
+
+colnames(plotting_data_no7)[2] <- "coef"
+plotting_data_no7$type <- "Regression"
+plotting_data_no7 <- cbind(plotting_data_no7, error_no7)
+colnames(plotting_data_no7)[4] <- "error"
+
+all_data <- rbind(plotting_data_no7, plotting_data_montgomery)
+
+upper<-(all_data$coef+ 1.96*all_data$error)
+lower<- (all_data$coef - 1.96*all_data$error)
+
+ggplot(all_data, aes(fill=  type , x = Names_no7, y = coef))  +
+  geom_bar(position=position_dodge(), stat="identity", colour='black') +
+  geom_errorbar(aes(ymin= lower, ymax= upper), width=.2,position=position_dodge(.9)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))+ 
+    labs(title = "Model Weight and Error Comparison ")+
+   ylab("Model Weights")+
+    xlab("Model")
+
+# +
+#geom_errorbar(aes(ymax = upper_montgomery, ymin = lower_montgomery))  +
+ # theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))+ 
+#  labs(title = "Model weights of  EBMA")+
+ # ylab("Model Weights")+
+#  xlab("Model")
+###############
+
+
+# create a dataset
+specie=c(rep("sorgho" , 3) , rep("poacee" , 3) , rep("banana" , 3) , rep("triticum" , 3) )
+condition=rep(c("normal" , "stress" , "Nitrogen") , 4)
+value=abs(rnorm(12 , 0 , 15))
+data=data.frame(specie,condition,value)
+
+ggplot(data, aes(fill=condition, y=value, x=specie)) +
+  geom_bar(position="dodge", stat="identity")
+
+
+
+
+
